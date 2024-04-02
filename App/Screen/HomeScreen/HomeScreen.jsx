7be1fff2d3422c9af83b0140a994e82d5d4c007b
someone <onlyiqquee@gmail.com>
2024-04-02 22:42:32 +0100

    inline styling changed to external styling

--- App/Screen/HomeScreen/HomeScreen.jsx
+++ App/Screen/HomeScreen/HomeScreen.jsx
@@ -1,208 +1,47 @@
 import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
-import Color from "../../Utils/Colors"
-import { useFonts } from 'expo-font';
 import Colors from '../../Utils/Colors';
 
 export default function HomeScreen() {
     return (
         <View>
-            <View>
-                <Text style={{
-                    color: Color.GRAY,
-                    fontSize: 18,
-                    fontFamily: "outfit"
-                    }}>
-                        Welcome Back!!!
-                    </Text>
-            </View>
-            <View>
-                <Text style={{
-                    fontSize: 35,
-                    width: "60%",
-                    fontFamily:"outfit-medium",
-                }}>
-                    Manage Your Daily Task
-                </Text>
-            </View>
+            <Text style={styles.welcomeBack}>Welcome Back!!!</Text>
+            <Text style={styles.manageDailyTask}>Manage Your Daily Task</Text>
 
-            <View style={styles.colonList}>
+            <View style={styles.overview}>
                 {/* first div */}
-                <View style={{
-                    backgroundColor: Colors.PURPLE,
-                    width: "48%",
-                    padding: 15,
-                    borderRadius: 20,
-                }}>
-                    <View style={{
-                            width: "60%",
-                            alignItems: "center",
-                            backgroundColor: Color.WHITE,
-                            borderRadius: 20,
-                            padding: 10,
-                        }}>
-                            <Image source={require("../../../assets/images/good1.png")}
-                                style={{
-                                    objectFit: "contain",
-                                    width: 60,
-                                    height:60,
-                                }}
-                            />
+                <View style={styles.firstGrid}>
+                    <View style={styles.goodMarker}>
+                        <Image style={styles.firstGridImage} source={require("../../../assets/images/good1.png")} />
                     </View> 
-
-                    <Text style={{
-                        marginTop: 20,
-                        color: Colors.WHITE,
-                        fontFamily: "outfit-medium",
-                        fontSize: 23,
-                    }}>
-                        App Branding Redesign
-                    </Text>
-                    <View style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "center",
-                    }}>
-                        <Image source={require("../../../assets/images/task-list-icon.png")}
-                        style={{
-                            objectFit: "contain",
-                            width: 15,
-                            height:15,
-                        }}
-                        />
-                        <Text style={{
-                            color: Colors.WHITE,
-                            marginLeft: 5,
-                            fontSize: 14,
-                        }}>10 task</Text>
+                    <Text style={styles.firstGridTaskName}>App Branding Redesign</Text>
+                    <View style={styles.firstGridSubTaskSection}>
+                        <Image style={styles.firstGridTaskListImage} source={require("../../../assets/images/task-list-icon.png")} />
+                        <Text style={styles.numberOfCompletedSubTasks}>10 sub task(s)</Text>
                     </View>
                 </View>
                 {/* first div ends */}
 
                 {/* second div */}
-                <View style={{
-                    backgroundColor: Colors.YELLOW,
-                    width: "48%",
-                    padding: 15,
-                    borderRadius: 20,
-                    height: 85
-                }}>
+                <View style={styles.completedTaskSection}>
+                <View >
                     <View>
-                            <Image source={require("../../../assets/images/clock.png")}
-                                style={{
-                                    objectFit: "contain",
-                                    width: 60,
-                                    height:60,
-                                    position: "absolute",
-                                    right: -5,
-                                    top: -40
-                                }}
-                            />
-                    </View> 
-
-                    <View >
-                        <Text style={{
-                            color: Colors.WHITE,
-                            fontFamily: "outfit-medium",
-                            fontSize: 50,
-                        }}>
-                            15
-                        </Text>
-                        <Text style={{
-                            color: Colors.WHITE,
-                            fontFamily: "outfit-medium",
-                            position: "absolute",
-                            bottom: 10,
-                            left: 40,
-                        }}>
-                            Completed
-                        </Text>
+                        <Image style={styles.clockImage} source={require("../../../assets/images/clock.png")} />
                     </View>
+                    <View >
+                        <Text style={styles.completedTaskNumber}>15</Text>
+                        <Text style={styles.completedTaskText}>Completed</Text>
+                    </View>
+                </View>
+
+                <View style={styles.availableTaskSection}>
+                    <View >
+                        <Text style={styles.availableTaskNumber}>15</Text>
+                        <Text style={styles.availableTaskText}>Available</Text>
+                    </View>
+                </View>
                 </View>
                 {/* second div ends */}
-
-                {/* third div */}
-                <View style={{
-                    backgroundColor: Colors.BLUE,
-                    width: "48%",
-                    padding: 15,
-                    borderRadius: 20,
-                    height: 85,
-                    marginTop: 15
-                }}>
-
-                    <View >
-                        <Text style={{
-                            color: Colors.WHITE,
-                            fontFamily: "outfit-medium",
-                            fontSize: 50,
-                        }}>
-                            15
-                        </Text>
-                        <Text style={{
-                            color: Colors.WHITE,
-                            fontFamily: "outfit-medium",
-                            position: "absolute",
-                            bottom: 10,
-                            left: 40,
-                        }}>
-                            Available
-                        </Text>
-                    </View>
-                </View>
-                {/* third div ends */}
-                
-                 {/* forth div */}
-                <View style={{
-                    backgroundColor: Colors.PURPLE,
-                    width: "48%",
-                    padding: 15,
-                    borderRadius: 20,
-                }}>
-                    <View style={{
-                            width: "60%",
-                            alignItems: "center",
-                            backgroundColor: Color.WHITE,
-                            borderRadius: 20,
-                            padding: 10,
-                        }}>
-                            <Image source={require("../../../assets/images/active-task.png")}
-                                style={{
-                                    objectFit: "contain",
-                                    width: 60,
-                                    height:60,
-                                }}
-                            />
-                    </View> 
-
-                    <Text style={{
-                        marginTop: 20,
-                        color: Colors.WHITE,
-                        fontFamily: "outfit-medium",
-                        fontSize: 23,
-                    }}>
-                        Upcoming Task
-                    </Text>
-                    <View style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "center",
-                    }}>
-                        <Image source={require("../../../assets/images/task-list-icon.png")}
-                        style={{
-                            objectFit: "contain",
-                            width: 15,
-                            height:15,
-                        }}
-                        />
-                        <Text style={{
-                            color: Colors.WHITE,
-                            marginLeft: 5,
-                            fontSize: 14,
-                        }}>10 task</Text>
-                    </View>
-                </View>
-                {/* forth div ends */}
             </View>
         
         </View>
@@ -210,11 +49,107 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-    colonList: {
+    welcomeBack: {
+        color: Colors.GRAY,
+        fontSize: 18,
+        fontFamily: "outfit"
+    },
+    manageDailyTask: {
+        fontSize: 35,
+        width: "60%",
+        fontFamily:"outfit-medium",
+    },
+    overview: {
         display: "flex",
         flexDirection: "row",
         flexWrap: "wrap",
         marginTop: 20,
         justifyContent: "space-between"
+    },
+    firstGrid: {
+        backgroundColor: Colors.PURPLE,
+        width: "48%",
+        padding: 15,
+        borderRadius: 20,
+    },
+    goodMarker: {
+        width: "60%",
+        alignItems: "center",
+        backgroundColor: Colors.WHITE,
+        borderRadius: 20,
+        padding: 10,
+    },
+    firstGridImage: {
+        objectFit: "contain",
+        width: 60,
+        height:60,
+    },
+    firstGridTaskName: {
+        marginTop: 20,
+        color: Colors.WHITE,
+        fontFamily: "outfit-medium",
+        fontSize: 23,
+    },
+    firstGridSubTaskSection:{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    firstGridTaskListImage: {
+        objectFit: "contain",
+        width: 15,
+        height:15,
+    },
+    numberOfCompletedSubTasks: {
+        color: Colors.WHITE,
+        marginLeft: 5,
+        fontSize: 14,
+    },
+    completedTaskSection: {
+        backgroundColor: Colors.YELLOW,
+        width: "48%",
+        padding: 15,
+        borderRadius: 20,
+        height: 85
+    },
+    clockImage: {
+        objectFit: "contain",
+        width: 60,
+        height:60,
+        position: "absolute",
+        right: -5,
+        top: -40
+    },
+    completedTaskNumber: {
+        color: Colors.WHITE,
+        fontFamily: "outfit-medium",
+        fontSize: 50,
+    },
+    completedTaskText: {
+        color: Colors.WHITE,
+        fontFamily: "outfit-medium",
+        position: "absolute",
+        bottom: 10,
+        left: 40,
+    },
+    availableTaskSection: {
+        backgroundColor: Colors.BLUE,
+        width: "48%",
+        padding: 15,
+        borderRadius: 20,
+        height: 85,
+        marginTop: 15
+    },
+    availableTaskNumber: {
+        color: Colors.WHITE,
+        fontFamily: "outfit-medium",
+        fontSize: 50,
+    },
+    availableTaskText: {
+        color: Colors.WHITE,
+        fontFamily: "outfit-medium",
+        position: "absolute",
+        bottom: 10,
+        left: 40,
     }
 })
