4fb447fccfe4a1c8b37b1b4036107359d2a75c04
someone <onlyiqquee@gmail.com>
2024-04-10 10:27:16 +0100

    theme added to create task

--- app/createtask.jsx
+++ app/createtask.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, KeyboardAvoidingView, Platform, TextInput, Image, TouchableOpacity, useWindowDimensions } from 'react-native'
+import { View, Text, StyleSheet, KeyboardAvoidingView, Platform, TextInput, Image, TouchableOpacity, useWindowDimensions, ScrollView } from 'react-native'
 import Colors from './Utils/Colors';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import React, { useState } from "react";
@@ -27,64 +27,108 @@ export default function CreateTask() {
     return (
         <View style={{ padding: 10, height: height }}>
             <StatusBar style="dark" />
-            <Text style={styles.header}>Create New Task</Text>
-            <View style={styles.wrapper}>
-                <View>
-                    <Text style={styles.taskName}>Task Name</Text>
-                    <KeyboardAvoidingView
-                        behavior={Platform.OS === "ios" ? "padding" : "height"}
-                        style={styles.writeTaskWrapper} >
-                        <TextInput style={styles.input} placeholder={"Input your task name"} placeholderTextColor="grey" />
-                    </KeyboardAvoidingView>
-                </View>
+            <View style={styles.themeIdentifier}></View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Text style={styles.header}>Create New Task</Text>
+                <View style={styles.wrapper}>
+                    <View>
+                        <Text style={styles.taskName}>Task Name</Text>
+                        <KeyboardAvoidingView
+                            behavior={Platform.OS === "ios" ? "padding" : "height"}
+                            style={styles.writeTaskWrapper} >
+                            <TextInput style={styles.input} placeholder={"Input your task name"} placeholderTextColor="grey" />
+                        </KeyboardAvoidingView>
+                    </View>
 
-                <View>
-                    <Text style={styles.subTaskName}>Sub Task(s)</Text>
-                    <KeyboardAvoidingView
-                        behavior={Platform.OS === "ios" ? "padding" : "height"}
-                        style={styles.writeTaskWrapper} >
-                        <TextInput style={styles.input} placeholder={"Input your sub task"} placeholderTextColor="grey" />
-                    </KeyboardAvoidingView>
-                    <KeyboardAvoidingView
-                        behavior={Platform.OS === "ios" ? "padding" : "height"}
-                        style={styles.writeTaskWrapper} >
-                        <TextInput style={styles.input} placeholder={"Input your sub task"} placeholderTextColor="grey" />
-                    </KeyboardAvoidingView>
+                    <View>
+                        <Text style={styles.subTaskName}>Sub Task(s)</Text>
+                        <KeyboardAvoidingView
+                            behavior={Platform.OS === "ios" ? "padding" : "height"}
+                            style={styles.writeTaskWrapper} >
+                            <TextInput style={styles.input} placeholder={"Input your sub task"} placeholderTextColor="grey" />
+                        </KeyboardAvoidingView>
+                        <KeyboardAvoidingView
+                            behavior={Platform.OS === "ios" ? "padding" : "height"}
+                            style={styles.writeTaskWrapper} >
+                            <TextInput style={styles.input} placeholder={"Input your sub task"} placeholderTextColor="grey" />
+                        </KeyboardAvoidingView>
+                        <KeyboardAvoidingView
+                            behavior={Platform.OS === "ios" ? "padding" : "height"}
+                            style={styles.writeTaskWrapper} >
+                            <TextInput style={styles.input} placeholder={"Input your sub task"} placeholderTextColor="grey" />
+                        </KeyboardAvoidingView>
+                        <KeyboardAvoidingView
+                            behavior={Platform.OS === "ios" ? "padding" : "height"}
+                            style={styles.writeTaskWrapper} >
+                            <TextInput style={styles.input} placeholder={"Input your sub task"} placeholderTextColor="grey" />
+                        </KeyboardAvoidingView>
+                        <KeyboardAvoidingView
+                            behavior={Platform.OS === "ios" ? "padding" : "height"}
+                            style={styles.writeTaskWrapper} >
+                            <TextInput style={styles.input} placeholder={"Input your sub task"} placeholderTextColor="grey" />
+                        </KeyboardAvoidingView>
 
-                    <TouchableOpacity style={{width: "25%"}}>
-                        <Text style={styles.addSubTask}>Add Sub Task</Text>
-                    </TouchableOpacity>
-                </View>
-
-                <View>
-                    <Text style={[styles.selectTimeTitle, styles.taskName]}>Select Time and Date</Text>
-                    <View style={styles.dateTime}>
-                        <Text style={styles.dateTimeValue}>{dateTimeValue ? dateTimeValue.toTimeString : "2025-09-06 12:15 AM"}</Text>
-                        <TouchableOpacity activeOpacity={1} onPress={showDatePicker}>
-                            <Image style={styles.calenderImage} source={require("../assets/images/calender.png")} />
+                        <TouchableOpacity style={{ width: "25%" }}>
+                            <Text style={styles.addSubTask}>Add Sub Task</Text>
                         </TouchableOpacity>
                     </View>
 
-                    <DateTimePickerModal
-                        isVisible={isDateTimePickerVisible}
-                        mode="datetime"
-                        onConfirm={handleConfirm}
-                        onCancel={hideDatePicker}
-                        themeVariant="dark"
-                    // positiveButton={{label: 'OK', textColor: 'green'}}
-                    />
-                </View>
+                    <View>
+                        <Text style={[styles.selectTimeTitle, styles.taskName]}>Select Time and Date</Text>
+                        <View style={styles.dateTime}>
+                            <Text style={styles.dateTimeValue}>{dateTimeValue ? dateTimeValue.toTimeString : "2025-09-06 12:15 AM"}</Text>
+                            <TouchableOpacity activeOpacity={1} onPress={showDatePicker}>
+                                <Image style={styles.calenderImage} source={require("../assets/images/calender.png")} />
+                            </TouchableOpacity>
+                        </View>
 
-                <View>
-                    <Text style={[styles.taskDescription, styles.taskName]}>Description</Text>
-                    {/* <KeyboardAvoidingView
+                        <DateTimePickerModal
+                            isVisible={isDateTimePickerVisible}
+                            mode="datetime"
+                            onConfirm={handleConfirm}
+                            onCancel={hideDatePicker}
+                            themeVariant="dark"
+                        // positiveButton={{label: 'OK', textColor: 'green'}}
+                        />
+                    </View>
+
+                    <View>
+                        <Text style={[styles.taskDescription, styles.taskName]}>Description</Text>
+                        {/* <KeyboardAvoidingView
                         behavior={Platform.OS === "ios" ? "padding" : "height"}
                         style={styles.writeTaskWrapper} > */}
-                    <TextInput multiline={true} numberOfLines={10} placeholder={"Input your task description..."}
-                        placeholderTextColor="grey" style={[styles.input, styles.descriptionInput]} />
-                    {/* </KeyboardAvoidingView> */}
+                        <TextInput multiline={true} numberOfLines={10} placeholder={"Input your task description..."}
+                            placeholderTextColor="grey" style={[styles.input, styles.descriptionInput]} />
+                        {/* </KeyboardAvoidingView> */}
+                    </View>
+
+                    {/* select theme */}
+                    <View>
+                        <Text style={[styles.taskDescription, styles.taskName]}>Select Theme</Text>
+                        <View style={styles.themeContainer}>
+                            {/* purple theme */}
+                            <TouchableOpacity>
+                                <View style={styles.purpleTheme}></View>
+                            </TouchableOpacity>
+                            {/* yellow theme */}
+                            <TouchableOpacity>
+                                <View style={styles.yellowTheme}></View>
+                            </TouchableOpacity>
+                            {/* blue theme */}
+                            <TouchableOpacity>
+                                <View style={styles.blueTheme}></View>
+                            </TouchableOpacity>
+                            {/* green theme */}
+                            <TouchableOpacity>
+                                <View style={styles.greenTheme}></View>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </View>
-            </View>
+
+                 {/* divider */}
+                <View style={{ marginVertical: 80 }}></View>
+            </ScrollView>
             <TouchableOpacity style={styles.createTask} activeOpacity={0.7}>
                 <Text style={styles.createTaskButton}>Create Task</Text>
             </TouchableOpacity>
@@ -93,6 +137,13 @@ export default function CreateTask() {
 }
 
 const styles = StyleSheet.create({
+    themeIdentifier: {
+        // width: Colors.PURPLE,
+        height: 5,
+        backgroundColor: Colors.PURPLE,
+        marginBottom: 10,
+        borderRadius: 10,
+    },
     header: {
         color: Colors.BLACKISH,
         fontFamily: "outfit-medium",
@@ -174,5 +225,37 @@ const styles = StyleSheet.create({
         textAlign: "center",
         fontFamily: "outfit-medium",
         fontSize: 15
-    }
+    },
+    themeContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        gap: 7,
+        flexWrap: "wrap",
+        marginTop: 10,
+    },
+    purpleTheme: {
+        backgroundColor: Colors.PURPLE,
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+    },
+    yellowTheme: {
+        backgroundColor: Colors.YELLOW,
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+    },
+    blueTheme: {
+        backgroundColor: Colors.BLUE,
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+    },
+    greenTheme: {
+        backgroundColor: "green",
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+    },
 })
